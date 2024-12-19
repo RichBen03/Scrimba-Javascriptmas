951a0e1f1bd428efad0f528ffb7d554772fd101f
Rich Benjamin <richmwendwa03@gmail.com>
2024-12-19 21:38:22 +0300

    Completed Day 6 of the Scrimba Challenge

---
+++ Day 6/index.js
@@ -0,0 +1,18 @@
+let anagrams = [
+    ["Can Assault", "Santa Claus"],
+    ["Refreshed Erudite Londoner", "Rudolf the Red Nose Reindeer"],
+    ["Frosty The Snowman", "Honesty Warms Front"],
+    ["Drastic Charms", "Christmas Cards"],
+    ["Congress Liar", "Carol Singers"],
+    ["The Tin Glints", "Silent Night"],
+    ["Be The Helm", "Betlehem"],
+    ["Is Car Thieves", "Christmas Eve"]
+];
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.join())
+
+const myArr = [[1,2],[3,4],[5,6]];
+const newArr = myArr.flat();
+
+
+console.log(newArr);
